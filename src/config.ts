e7815fca0e94c3d3e3176d3a332669f1078f6665
Vassilis Papapanagiotou <bpapapana@gmail.com>
2024-02-09 22:10:42 +0100

    First commit

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Vassilis Papapanagiotou personal website';
+export const SITE_DESCRIPTION = 'Welcome to Vassilis Papapanagiotou personal website. Featuring AI and blockchain, personal portfolio and blog. Also my CV :)';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
