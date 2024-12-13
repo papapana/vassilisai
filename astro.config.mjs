41334050c53c40461bc5b03708fdad0c9427a666
Vassilis Papapanagiotou <bpapapana@gmail.com>
2024-12-13 08:34:39 +0100

    - Testing JamComments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import {targetBlank} from "./src/plugins/targetBlank";
+import {jamComments} from "@jam-comments/astro/config";
+import "dotenv/config";
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,7 +15,13 @@ export default defineConfig({
       config: {
         forward: ["datalayer.push"]
       }
-    })],
+    }),
+    jamComments({
+      domain: process.env.JAM_COMMENTS_DOMAIN,
+      apiKey: process.env.JAM_COMMENTS_API_KEY,
+      environment: process.env.JAM_COMMENTS_ENVIRONMENT, // `production` or `development` // optional
+    })
+  ],
   markdown: {
     rehypePlugins: [[targetBlank, {domain: 'vassilis.ai'}]],
   }
