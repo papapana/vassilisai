bc3dbbe84c28d463e5ba5a8fdaa52a4681bef00b
Vassilis Papapanagiotou <bpapapana@gmail.com>
2024-12-15 20:27:11 +0100

    - Experiments with talkyard as a component

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import {targetBlank} from "./src/plugins/targetBlank";
-import {jamComments} from "@jam-comments/astro/config";
+
 import "dotenv/config";
 
 // https://astro.build/config
@@ -16,11 +16,6 @@ export default defineConfig({
         forward: ["datalayer.push"]
       }
     }),
-    jamComments({
-      domain: process.env.JAM_COMMENTS_DOMAIN,
-      apiKey: process.env.JAM_COMMENTS_API_KEY,
-      environment: process.env.JAM_COMMENTS_ENVIRONMENT, // `production` or `development` // optional
-    })
   ],
   markdown: {
     rehypePlugins: [[targetBlank, {domain: 'vassilis.ai'}]],
